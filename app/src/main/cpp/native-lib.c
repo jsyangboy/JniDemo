cc761b810aa2f0e5fa3fca86ebf04e32177840b1
yqy <tpyangqingyuan@163.com>
2019-11-03 23:11:20 +0800

    引入ffmpeg

--- app/src/main/cpp/native-lib.c
+++ app/src/main/cpp/native-lib.c
@@ -22,8 +22,25 @@ Java_itboy_jnidemo_MainActivity_stringFromJNI(
 #include <jni.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "libavcodec/avcodec.h"
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
+#include "libswresample/swresample.h"
+
+#include "libavutil/time.h"
+
+static AVPacket *gs_packet=NULL;
+static AVPacket *gs_packetTemp = NULL;
+static AVFormatContext *gs_pformat_ctx = NULL;
+static int videoIndex = -1, audioIndex = -1,waitkey = 1;
+static char gs_filename[1024];
+
 JNIEXPORT jstring JNICALL Java_itboy_jnidemo_MainActivity_stringFromJNI(
         JNIEnv* env,jobject cls) {
+    AVOutputFormat *of = av_guess_format( 0, "file://", 0 );
+    gs_pformat_ctx = avformat_alloc_context();
+
 #ifdef ENABLE_RTC
     return (*env)->NewStringUTF(env, "你好,RTC");
 #else
