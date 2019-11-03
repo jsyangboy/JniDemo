5346f25ad70682af01a7b042e898d8117e8a1665
yqy <tpyangqingyuan@163.com>
2019-11-03 17:23:47 +0800

    增加编译宏

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,7 +5,14 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_itboy_jnidemo_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
+
+#ifdef ENABLE_RTC
+    std::string hello = "Hello RTC";
+    return env->NewStringUTF(hello.c_str());
+#else
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+#endif
+
 }
 
